13be5ed151ca7b6fb88ec6428ddbf3b3d6380967
Gabriel Nguyen <gnguyen@et.esiea.fr>
2023-11-15 20:58:47 +0100

    init

--- main.py
+++ main.py
@@ -7,28 +7,28 @@ from tabulate import tabulate
 tabChar = "azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN&é-éù$:;123456789"
 
 
-def generation_clePrivee(difficulty):
-    clePrivee = []
+def generation_clePublique(difficulty):
+    clePublique = []
     for i in range(0, difficulty*4):
-        clePrivee.append(random.randint(1, 9))
+        clePublique.append(random.randint(1, 9))
         
-    return clePrivee
+    return clePublique
 
-def generation_clePublique(ternary_representations, clePrivee, difficulty):
+def generation_clePrivee(ternary_representations, clePublique, difficulty):
     g = []
     for i in range(0,difficulty*4):
         g.append(random.randint(-1, 1))
         
             
-    clePublique = []
+    clePrivee = []
     
     for i in range(0,difficulty*4):
-        temp = clePrivee[i] * g[i]
-        clePublique.append(temp)
+        temp = clePublique[i] * g[i]
+        clePrivee.append(temp)
         
-    return clePublique
+    return clePrivee
 
-def decimal_to_ternary_with_minus_one(decimal_number):
+def repTernaire(decimal_number):
     if decimal_number == 0:
         return "0"
 
@@ -86,46 +86,47 @@ def motToTernaire(mot):
     
     for i in range (0,len(mot)):
         pos = tabChar.index(mot[i])+1
-        ternaireTab = decimal_to_ternary_with_minus_one(pos)
+        ternaireTab = repTernaire(pos)
         ternaire.extend(ternaireTab)
         
     return ternaire
 
-def message_Chiffre(difficulty, clePublic, message):
-    tempTab = [i * difficulty for i in clePublic]
+def message_Chiffre(difficulty, clePrivee, message):
+    tempTab = [i * difficulty for i in clePrivee]
     messageChiffre = [i + j for i,j in zip(tempTab, message)]
     
     return messageChiffre
 
-def clePublicRightTurn(ClePublic):
-    temp = ClePublic[len(ClePublic)-1]
-    ClePublic[1:] = ClePublic[:-1]
-    ClePublic[0] = temp
-    return ClePublic
+def clePriveeRightTurn(ClePrivee):
+    temp = ClePrivee[len(ClePrivee)-1]
+    ClePrivee[1:] = ClePrivee[:-1]
+    ClePrivee[0] = temp
+    return ClePrivee
 
-def clePublicLeftTurn(ClePublic):
-    temp = ClePublic[0]
-    ClePublic[:-1] = ClePublic[1:]
-    ClePublic[len(ClePublic)-1] = temp
-    return ClePublic
+def clePriveeLeftTurn(ClePrivee):
+    temp = ClePrivee[0]
+    ClePrivee[:-1] = ClePrivee[1:]
+    ClePrivee[len(ClePrivee)-1] = temp
+    return ClePrivee
 
 
 
 mot,difficulty = motAleatoire("mots.txt")
-clePrivee = generation_clePrivee(difficulty)
+print(difficulty)
+clePublique = generation_clePublique(difficulty)
 
-ternary_representation = motToTernaire(mot)
+repTernaire = motToTernaire(mot)
 
-clePublic = generation_clePublique(ternary_representation, clePrivee, difficulty)
-message_Chiffre = message_Chiffre(difficulty, clePublic, ternary_representation)
+clePrivee = generation_clePrivee(repTernaire, clePublique, difficulty)
+message_Chiffre = message_Chiffre(difficulty, clePrivee, repTernaire)
 
-clePublicF = ["Cle Public"] + clePublic
-clePublicF = list(map(str,clePublicF))
+clePriveeF = ["Cle Privee"] + clePrivee
+clePriveeF = list(map(str,clePriveeF))
 
 message_ChiffreF = ["Message Chiffre"] + message_Chiffre
 message_ChiffreF = list(map(str,message_ChiffreF))
 
-tableaux = [clePublicF, message_ChiffreF]
+tableaux = [clePriveeF, message_ChiffreF]
 
 resultat = tabulate(tableaux, tablefmt="plain")
 
@@ -141,22 +142,22 @@ while not all(val in [1,-1,0] for val in message_Chiffre):
         
         
         if user_input == b'H':
-            clePublic[:] = np.array(clePublic) * -1
+            clePrivee[:] = np.array(clePrivee) * -1
         elif user_input == b'M':
-            clePublic = clePublicRightTurn(clePublic)
+            clePrivee = clePriveeRightTurn(clePrivee)
         elif user_input == b'K':
-            clePublic = clePublicLeftTurn(clePublic)
+            clePrivee = clePriveeLeftTurn(clePrivee)
         elif user_input == b'P':
-            message_Chiffre[:] = np.array(message_Chiffre) + np.array(clePublic)
+            message_Chiffre[:] = np.array(message_Chiffre) + np.array(clePrivee)
             
         
-        clePublicF = ["Cle Public"] + clePublic
-        clePublicF = list(map(str,clePublicF))
+        clePriveeF = ["Cle Privee"] + clePrivee
+        clePriveeF = list(map(str,clePriveeF))
         
         message_ChiffreF = ["Message Chiffre"] + message_Chiffre
         message_ChiffreF = list(map(str,message_ChiffreF))
 
-        tableaux = [clePublicF, message_ChiffreF]
+        tableaux = [clePriveeF, message_ChiffreF]
 
         resultat = tabulate(tableaux, tablefmt="plain")
 
